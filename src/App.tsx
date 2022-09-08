9a26b56b51ef2236b70e9d2dee1e34e79f01e9cd
DS <mailforgautamvg@gmail.com>
2022-09-08 11:31:51 +0530

    added basename to router

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,9 @@ import PostCustomMeme from './pages/PostCustomMeme';
 import PostTemplate from './pages/PostTemplate';
 
 const App = () => {
+	console.log(`public URL is: ${process.env.PUBLIC_URL}`);
 	return (
-		<BrowserRouter>
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
 			<div className='flex flex-col bg-zinc-900 w-screen min-h-screen'>
 				<div className='pt-4 px-4 flex flex-row gap-4 justify-evenly text-zinc-600 text-md md:justify-end'>
 					<Link to='/'>Memes</Link>
